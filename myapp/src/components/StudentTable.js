96824e60c0adef6c2b7f83fd051158fd4548c0b7
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-19 09:37:49 +0000

    Add initial structure for StudentTable component

--- myapp/src/components/StudentTable.js
+++ myapp/src/components/StudentTable.js
@@ -1,1 +1,19 @@
-rfce
+export default function StudentTable(props){
+    return(
+            <div className='outerDiv'>
+                <div className='innerDiv'>
+                    <table>
+                    
+
+                    </table>
+
+                </div>
+            </div>
+
+
+    )
+
+
+
+
+}
