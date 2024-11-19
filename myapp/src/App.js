77b99cdb6bd6fd929fd7c5cc9f162a338db2061d
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-19 09:42:29 +0000

    Import StudentTable component and update table header structure

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentTable from './components/StudentTable';
 
 function App() {
   return (
--- myapp/src/components/StudentTable.js
+++ myapp/src/components/StudentTable.js
@@ -3,7 +3,12 @@ export default function StudentTable(props){
             <div className='outerDiv'>
                 <div className='innerDiv'>
                     <table>
-                    
+                        <thread>
+                            <td>First Name</td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                        </thread>
 
                     </table>
 
